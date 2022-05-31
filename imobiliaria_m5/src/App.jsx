157d10b7f05edc16e72b1e4d6a79a93965ba0fee
joceliahora <joceliahora@gmail.com>
2022-05-31 18:24:35 -0300

    primeiro commmit jocelia

--- imobiliaria_m5/src/App.jsx
+++ imobiliaria_m5/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Header />
       //aqui começamos o show
     </div>
   )
---
+++ imobiliaria_m5/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.modules.css'
+
+const Header = () => {
+  return (
+    <div>!!!</div>
+  )
+}
+
+export default Header
