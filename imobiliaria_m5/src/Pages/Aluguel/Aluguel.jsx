dfa21cdac9638bb5989d55c8bf2ec8ca4e5c5711
Lucas <lukscalebe@hotmail.com>
2022-06-07 16:38:56 -0300

    verbos e consumo da api, entidade aluguel

--- imobiliaria_m5/src/Pages/Aluguel/Aluguel.jsx
+++ imobiliaria_m5/src/Pages/Aluguel/Aluguel.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Cabecalho from "../../Components/Cabecalho/Cabecalho";
+import Rodape from '../../Components/rodape/Rodape'
+import { Link } from "react-router-dom";
 
-const Aluguel = () => {
-  return (
-    <div>
-        <h1>Aluguel</h1>
-    </div>
-  )
+
+function Aluguel() {
+
+    const [aluguel, setAluguel] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://imobiliariaresilia.herokuapp.com/aluguel/')
+        .then((response) => {
+            setAluguel(response.data)
+            console.log('Deu Certo')
+        })
+        .catch(() => {
+            console.log('Deu Errado')
+        })
+    },[])
+
+    function deleteAluguel(id) {
+        axios.delete(`https://imobiliariaresilia.herokuapp.com/aluguel/${id}`)
+        
+        setAluguel(aluguel.filter(aluguel => aluguel.ID !== id))
+    }
+
+    return(
+        <div>
+            <Cabecalho />
+
+            <main>
+                <div className="cards">
+
+                    {aluguel.map((aluguel, key) => {
+                        return (
+                            <div className='card'>
+
+                                <header>
+                                    <h2>{aluguel.ALUGUELTIPO}</h2>                            
+                                </header>   
+
+                                <p>Valor: {aluguel.VALOR}</p>
+                                <p>Endereço: {aluguel.ENDERECO}</p>
+                                <p>ID Corretor: {aluguel.CORRETORID}</p>
+                                <p>ID Proprietario: {aluguel.PROPRIETARIOID}</p>
+                                <p>ID Inquilino: {aluguel.INQUILINOID}</p>
+                                {/*<img src={aluguel.IMG1} alt=''/>*/}
+
+                                <div className='btns'>
+                                    
+                                    <Link to={{ pathname: `/aluguelEdit/${aluguel.ID}` }}>
+                                        <div className='btn-edit'>
+                                            <button>Edit</button>
+                                        </div>
+                                    </Link>
+                                        
+
+                                    <div className='btn-delete'>
+                                        <button onClick={() => {deleteAluguel(aluguel.ID)} }>Delete</button>
+                                    </div>
+
+                            </div>
+                        </div> 
+                        )
+                    })}
+                    
+                </div>
+            </main>
+
+            <Link to={'/aluguelPost'}>
+                <button>Registrar Novo Imovel</button>
+            </Link>
+
+            <Rodape />
+        </div>
+    )
 }
 
-export default Aluguel
+export default Aluguel;
--- imobiliaria_m5/src/Routes/Routes.jsx
+++ imobiliaria_m5/src/Routes/Routes.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "../App";
+import Aluguel from "../Pages/Aluguel/Aluguel";
+import AluguelEdit from "../Pages/Aluguel/AluguelEdit";
+import AluguelPost from "../Pages/Aluguel/AluguelPost";
 import Vendas from "../Pages/Vendas/Vendas";
 import VendasEdit from "../Pages/Vendas/VendasEdit";
 import VendasPost from "../Pages/Vendas/VendasPost";
@@ -13,6 +16,10 @@ function Rotas() {
                 <Route path="/vendas" element={<Vendas />}></Route>
                 <Route path="/vendasPost" element={<VendasPost />}></Route>
                 <Route path="/vendasEdit/:id" element={<VendasEdit />}></Route>
+                <Route path="/aluguel" element={<Aluguel />}></Route>
+                <Route path="/aluguelPost" element={<AluguelPost />}></Route>
+                <Route path="/aluguelEdit/:id" element={<AluguelEdit />}></Route>
+
             </Routes>
         </Router>
     )
