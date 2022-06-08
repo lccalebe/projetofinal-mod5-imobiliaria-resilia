1b1745a0cabfa0e64af514d12f1c42b8ed0bf2e7
ElianeCristina <eliane_cristinanunescorreia@yahoo.com.br>
2022-06-08 02:25:08 -0300

    modificacao no texto do footer e correcao de identacao

--- imobiliaria_m5/src/Components/rodape/Rodape.jsx
+++ imobiliaria_m5/src/Components/rodape/Rodape.jsx
@@ -14,29 +14,27 @@ const Footer = () => {
                 <FaTwitter className={styles.icon} />
                 <FaPinterest className={styles.icon} />
             </div>
+            
             <div className={styles.container}>
                 <div className={styles.col}>
                     <h3> Home </h3>
                     <p> Empresa </p>
                     <p> Publicidade </p>
                     <p> Sustentabilidade </p>
-
                 </div>
+
                 <div className={styles.col}>
                     <h3> Cadastro </h3>
                     <p> Trabalhe Conosco </p>
                     <p> Filiais </p>
-                    <p> Equipe de Design</p>
-                    <p></p>
-
-
+                    <p> Franquias </p>
                 </div>
+
                 <div className={styles.col}>
                     <h3> Sobre </h3>
                     <p> Jurídico </p>
                     <p> Equipe de Design </p>
                     <p> Parcelamentos </p>
-
                 </div>
 
                 <div className={styles.col}>
